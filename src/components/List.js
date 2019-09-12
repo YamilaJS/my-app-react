2294b2d57f331ddb0dc026e8a2ad77866097c2c4
Yamila <yami.prog23@gmail.com>
2019-09-12 19:52:55 -0300

    first commit

---
+++ src/components/List.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const List = () => {
+    return(
+        <h1>Lista de Pokemones</h1>
+    );
+}
+
+export default List;
---
+++ src/containers/PokeListContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import List from '../components/List';
+
+class PokeListContainer extends Component {
+    render() {
+        return(
+            <List/>
+        );
+    }
+}
+
+    
+export default PokeListContainer;
