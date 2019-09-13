8623855835e122431792614e9e5eecdf51a44376
Leonardo Larrosa <llarrosa@digitalhouse.com>
2019-09-12 22:04:55 -0300

    Add query to Poke Api in PokeLIstContainer

--- src/containers/PokeListContainer.js
+++ src/containers/PokeListContainer.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import List from '../components/List';
 
 class PokeListContainer extends Component {
+    state = {
+        pokeData:  []
+    }
+
+    componentDidMount() {
+        axios.get('https://pokeapi.co/api/v2/pokemon')
+        .then(res => {
+            const pokeData = res.data.results;
+
+            this.setState({
+                pokeData
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+
     render() {
+
+        const { pokeData } = this.state;
+
         return(
-            <List/>
+            <List pokedata={ pokeData }/>
         );
     }
 }
