83cda29335ecdbcbb241c51bb9e885333df89bd1
Yamila <yami.prog23@gmail.com>
2019-09-13 17:39:58 -0300

    Add Card Component y styles

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,17 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
+import PokeCard from './PokeCard';
+import { Grid } from '@material-ui/core';
 
-function List ({ pokedata })  {
-    return(
+function List({ pokedata }) {
+    return (
         <Fragment>
-            <h1>Lista de Pokemons</h1>
-            <ul>
+            <Grid container justify="center">
                 {pokedata.map((pokemon, index) => {
-                    return (
-                    <li key= {index}>
-                     {pokemon.name}
-                     </li>
-                     );
+                    let url = "https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/"
+                    let pokeIndex = pokemon.url.split('/')[pokemon.url.split('/').length - 2]
+                    return <PokeCard name={pokemon.name} image={`${url}${pokeIndex}.png?raw=true`} />
                 })}
-            </ul>
+            </Grid>
         </Fragment>
     );
 }
